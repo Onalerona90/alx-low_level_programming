f1d842f106fde49a1aedf8740bc598aafb7d562a
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-23 20:57:26 +0200

    Dynamic library functions

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit.
+ * @c: The parameter checked.
+ *
+ * Return: 1 if c is a digit and 0 otherwise.
+*/
+
+int _isdigit(int c)
+{
+
+	if (isdigit(c))
+	{
+		return (1);
+	}
+
+	return (0);
+
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @v: The value computed.
+ *
+ * Return: The value computed.
+*/
+
+int _abs(int v)
+{
+if (v < 0)
+{
+v *= -1;
+}
+
+return (v);
+}
